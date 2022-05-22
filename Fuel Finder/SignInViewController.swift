87f1fb5764f97d927c1a2d7799b081b45972e39e
dharmikM <75500853+dharmikM@users.noreply.github.com>
2022-05-21 22:54:49 -0400

    First commit

---
+++ Fuel Finder/SignInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewController.swift
+//  Fuel Finder
+//
+//  Created by D M on 2022-05-21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    
+    //MARK: Outlets
+    //----------------
+    
+    
+    //MARK: Properties
+    //----------------
+    
+    
+    //MARK: Action
+    //----------------
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        
+    }
+    
+
+
+}
--- Fuel Finder/ViewController.swift
+++ Fuel Finder/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //MARK: Properties
+    //----------------
+    var defaults:UserDefaults = UserDefaults.standard
+      
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       checkFirstLaunch()
+       
     }
+    
+  
+    func checkFirstLaunch(){
+        let firstLaunchCheck:String? = self.defaults.string(forKey: "KEY_FIRST_LAUNCH")
+            if (firstLaunchCheck != nil ){
+                
+                guard let toSignInScreen = storyboard?.instantiateViewController(withIdentifier: "signInScreen")else{
+                    print("Signup Screen not found")
+                    return
+                }
+                
+                self.navigationController?.pushViewController(toSignInScreen, animated: true)
+                        
+            }
+    }
+    
+    //MARK: Action
+    //----------------
 
-
+    
+    @IBAction func getStartedBtn(_ sender: Any) {
+ 
+                defaults.set("true", forKey: "KEY_FIRST_LAUNCH")
+                
+                guard let toSignInScreen = storyboard?.instantiateViewController(withIdentifier: "signInScreen")else{
+                    print("Signup Screen not found")
+                    return
+                }
+                
+                self.navigationController?.pushViewController(toSignInScreen, animated: true)
+            }
+     
 }
 
